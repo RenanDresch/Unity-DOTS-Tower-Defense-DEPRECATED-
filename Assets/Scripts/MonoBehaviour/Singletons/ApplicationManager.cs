d4ccd229f1e5ba22be73fe9de6dc1dd86f92561a
Renan Dresch <renan@medroom.com.br>
2020-06-09 19:49:27 -0300

    Defaults autoload

--- Assets/Scripts/MonoBehaviour/Singletons/ApplicationManager.cs
+++ Assets/Scripts/MonoBehaviour/Singletons/ApplicationManager.cs
@@ -55,11 +55,6 @@ public class ApplicationManager : MonoBehaviour
     private void Awake()
     {
         _instance = this;
-
-#if !UNITY_EDITOR
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-#endif
-
     }
     public void SwitchLoader(bool state, System.Action callback = null)
     {
---
+++ Assets/Scripts/StartupManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupManager
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void OnRuntimeMethodLoad()
+    {
+        if(!SceneManager.GetSceneByName("Defaults").isLoaded)
+        {
+            SceneManager.LoadScene("Defaults", LoadSceneMode.Additive);
+        }
+    }
+}
